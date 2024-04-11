1939932b236a5ce51482ff6ccc9c41eaedbca35b
Pronin Denis <denis.pronin@sibers.com>
2024-04-11 17:00:22 +0700

    endVer

--- game-logic.js
+++ game-logic.js
@@ -1,60 +1,38 @@
 const sizeField = 3
-const quantityCross = Math.ceil(sizeField * sizeField / 2)
-const quantityZero = Math.floor(sizeField * sizeField / 2)
 
 let step = 0
 let arrayCross = []
 let arrayZero = []
 
+// initgame
 function createVariable() {
     step = 0
     arrayCross = []
     arrayZero = []
 }
 
-function createArray(arr, quantityNum) {
-    arr = new Array(quantityNum)
-    for (let i = 0; i < quantityNum; i++) {
-        arr[i] = new Array(2)
-        for (let j = 0; j < 2; j++) {
-            arr[i][j] = 0
-        }
-    }
-    return arr
-}
-
 function movePlayer(x, y) {
-    let combinationVictory
     drawLetter(x, y)
     lockButtonAfterPressing(x, y)
     changeArrayOfMoves(x, y)
-    if (step % 2 == 0) {
-        combinationVictory = checkVictoryCondition(arrayCross.length, arrayCross)
-        if (combinationVictory != undefined) {
-            paintCellAfterWin(combinationVictory)
-            alertVicory("победил 1-ый")
-            lockButton()
-        } else if (combinationVictory == undefined && step == 8) {
-            alertVicory("ничья")
-            lockButton()
-        }
-    } else {
-        combinationVictory = checkVictoryCondition(arrayZero.length, arrayZero)
-        if (combinationVictory != undefined) {
-            paintCellAfterWin(combinationVictory)
-            alertVicory("победил 2-ой")
-            lockButton()
-        }
+    const currentArray = step % 2 === 0 ? arrayCross : arrayZero
+    const currentMove = step % 2 + 1
+
+    const combinationVictory = checkVictoryCondition(currentArray.length, arrayCross)
+    if (combinationVictory !== undefined) {
+        paintCellAfterWin(combinationVictory)
+        alertVictory(`победил ${currentMove}-ый`)
+        lockButton()
+    } else if (combinationVictory === undefined && step === 8) {
+        alertVictory("ничья")
+        lockButton()
     }
     step += 1
 }
 
 function drawLetter(x, y) {
-    if (step % 2 == 0) {
-        document.getElementById("img" + x + y).src = '/img/cross.png'
-    } else {
-        document.getElementById("img" + x + y).src = '/img/zero.png'
-    }
+    const letter = step % 2 == 0 ? `cross` : `zero`
+    document.getElementById("img" + x + y).src = `/img/${letter}.png`
 }
 
 function lockButtonAfterPressing(x, y) {
@@ -62,11 +40,7 @@ function lockButtonAfterPressing(x, y) {
 }
 
 function changeArrayOfMoves(x, y) {
-    if (step % 2 == 0) {
-        arrayCross.push([x,y])
-    } else {
-        arrayZero.push([x,y])
-    }
+    step % 2 == 0 ? arrayCross.push([x,y]) : arrayZero.push([x,y])
 }
 
 function checkVictoryCondition(quantity, arr) {
@@ -75,7 +49,7 @@ function checkVictoryCondition(quantity, arr) {
         for (let i = 0; i < quantity - 2; i++) {
             for (let j = i + 1; j < quantity - 1; j++) {
                 for (let k = j + 1; k < quantity; k++) {
-                    if (calculationArea(arr[i], arr[j], arr[k]) == 0) {
+                    if (calculationArea(arr[i], arr[j], arr[k]) === 0) {
                         return combinationVictory = [arr[i], arr[j], arr[k]]
                     }
                 }
@@ -86,7 +60,7 @@ function checkVictoryCondition(quantity, arr) {
 }
 
 function calculationArea(a, b, c) {
-    return ((b[0] - a[0])*(c[1] - a[1]) - (c[0] - a[0])*(b[1] - a[1])) * 0.5
+    return ((b[0] - a[0]) * (c[1] - a[1]) - (c[0] - a[0]) * (b[1] - a[1])) * 0.5
 }
 
 function paintCellAfterWin(arr) {
@@ -95,7 +69,7 @@ function paintCellAfterWin(arr) {
     }
 }
 
-function alertVicory(num) {
+function alertVictory(num) {
     document.getElementById("form").textContent = num
     document.getElementById("form").style.visibility = "visible"
 }
